71958cec11b38093278441bd8c937992a0626818
Maksym Prysiazhnyi <m.prysiazhnyi@gmail.com>
2018-01-15 19:44:21 +0100

    Add generic rest controller

--- src/main/java/com/phpdaddy/eshopibm/controller/RestController.java
+++ src/main/java/com/phpdaddy/eshopibm/controller/RestController.java
@@ -1,13 +1,14 @@
 package com.phpdaddy.eshopibm.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.io.Serializable;
 import java.util.HashMap;
