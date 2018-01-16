89757e0c6729b100bae8ad450f922183043af157
Maksym Prysiazhnyi <m.prysiazhnyi@gmail.com>
2018-01-16 01:40:18 +0100

    Add persistence config

---
+++ src/main/java/com/phpdaddy/eshopibm/PersistenceConfig.java
@@ -0,0 +1,32 @@
+package com.phpdaddy.eshopibm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan({"com.phpdaddy.eshopibm"})
+public class PersistenceConfig {
+    @Bean
+    public LocalEntityManagerFactoryBean entityManagerFactory() {
+        LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
+        factoryBean.setPersistenceUnitName("LOCAL_PERSISTENCE");
+        return factoryBean;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+}
+
